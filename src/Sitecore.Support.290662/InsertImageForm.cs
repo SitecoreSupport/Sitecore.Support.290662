891d4505faab1296d36378197f9f1c7cc604c4e0
iro <iro@sitecore.net>
2018-11-05 21:52:18 +0200

    refactor the code to make the fix

--- src/Sitecore.Support.290662/InsertImageForm.cs
+++ src/Sitecore.Support.290662/InsertImageForm.cs
@@ -14,8 +14,41 @@
   {
     private Item GetCurrentItem(Message message)
     {
-      return GetCurrentItem(DataContext, message, DataContext.Language);
+      //change the call of the base method to make inheritance and code change easier and more reliable
+      return this.GetCurrentItem(DataContext, message, DataContext.Language);
     }
+
+    #region Added code from MediaDialogFormBase
+    protected new Item GetCurrentItem(DataContext context, Message message, Language language)
+    {
+      Assert.ArgumentNotNull(message, "message");
+      string text = message["id"];
+      Item folder = context.GetFolder();
+      if (folder != null)
+      {
+        language = folder.Language;
+      }
+      if (!string.IsNullOrEmpty(text))
+      {
+        return Client.ContentDatabase.GetItem(text, language);
+      }
+      if (folder != null && context.Folder != folder.ID.ToString())
+      {
+        Item[] virtualChildren = folder.GetVirtualChildren();
+        if (virtualChildren.Any())
+        {
+          #region Modified code
+          Item selectedItem = folder.Database.GetItem(context.Folder);
+          #endregion
+          if (selectedItem != null && virtualChildren.Any((Item v) => v.Axes.IsAncestorOf(selectedItem)))
+          {
+            return selectedItem;
+          }
+        }
+      }
+      return folder;
+    }
+    #endregion
 
     public override void HandleMessage(Message message)
     {
