84aec5de7cae9f806146457762a9e2f09c00a0a1
iro <iro@sitecore.net>
2018-11-05 21:43:19 +0200

    Initial

---
+++ src/Sitecore.Support.290662/InsertImageForm.cs
@@ -0,0 +1,36 @@
+﻿namespace Sitecore.Support.XA.Foundation.Multisite.Dialogs
+{
+  using Sitecore;
+  using Sitecore.Data.Items;
+  using Sitecore.Diagnostics;
+  using Sitecore.Globalization;
+  using Sitecore.Shell.Framework;
+  using Sitecore.Web.UI.HtmlControls;
+  using Sitecore.Web.UI.Sheer;
+  using Sitecore.XA.Foundation.Multisite.Extensions;
+  using System.Linq;
+
+  public class InsertImageForm : Sitecore.XA.Foundation.Multisite.Dialogs.InsertImageForm
+  {
+    private Item GetCurrentItem(Message message)
+    {
+      return GetCurrentItem(DataContext, message, DataContext.Language);
+    }
+
+    public override void HandleMessage(Message message)
+    {
+      Assert.ArgumentNotNull(message, "message");
+      if (message.Name == "item:load")
+      {
+        #region Modified code
+        base.HandleMessage(message);
+        #endregion
+      }
+      else
+      {
+        Dispatcher.Dispatch(message, GetCurrentItem(message));
+        base.HandleMessage(message);
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.290662/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.290662")]
+[assembly: AssemblyProduct("Sitecore.Support.290662")]
+[assembly: ComVisible(false)]
